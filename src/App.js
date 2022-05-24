5fbb025df679e4fa7496866cce05be51f29c8225
Adrian <adriantutuianu01@gmail.com>
2022-05-24 23:33:19 +0100

    Input added for ammount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    const result = event.target.value.replace(/\D/g, "");
+
+    setValue(result);
+  };
+
+  console.log(Number(value));
+
   return (
     <div className="App">
-      <main className="content"></main>
+      <main className="content">
+        <div className="ammount">
+          <p>Ammount</p>
+          <input
+            type="text"
+            placeholder=""
+            value={value}
+            onChange={handleChange}
+          />
+        </div>
+      </main>
     </div>
   );
 }
