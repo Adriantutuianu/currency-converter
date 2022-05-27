9e847c7ddfe4c2d5b11454f819ee9bf33c210286
Adrian <adriantutuianu01@gmail.com>
2022-05-28 00:28:54 +0100

    Fetched currency list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [amountValue, setAmountValue] = useState("");
+  const [currencies, setCurrencies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getCurrencies = async () => {
+    setLoading(true);
+
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", process.env.REACT_APP_API_KEY);
+
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    };
+
+    try {
+      await fetch("https://api.apilayer.com/currency_data/list", requestOptions)
+        .then((res) => res.json())
+        .then((res) => {
+          setCurrencies(res.currencies);
+        });
+    } catch (error) {
+      console.log("Failed to get all currencies: " + error);
+    }
+    setLoading(false);
+  };
 
   const handleChangeAmount = (event) => {
     setAmountValue(Number(event.target.value));
   };
 
-  console.log(amountValue);
+  useEffect(() => {
+    getCurrencies();
+  }, []);
 
   return (
     <div className="App">
@@ -17,7 +45,7 @@ function App() {
           <p>Amount</p>
           <input
             type="number"
-            placeholder=""
+            placeholder="0"
             value={amountValue}
             onChange={handleChangeAmount}
           />
