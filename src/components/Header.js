59ca51fc348a412bb7454ecb022cbfe3e8e9c74e
Adrian <adriantutuianu01@gmail.com>
2022-06-14 12:02:34 +0100

    Styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <h1>Currency converter</h1>;
+  return (
+    <header>
+      <h1>Currency converter</h1>
+    </header>
+  );
 };
 
 export default Header;
